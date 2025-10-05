ab9e2bf0198f033a89924fefb9587c57c70ca9e5
unknown <gautamdtua42003@gmail.com>
2025-10-06 00:50:10 +0530

    render deploy

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:5000";
+const API = "https://notes-app-3-8hk7.onrender.com/";
 
 // -------------------- LOGIN & REGISTER --------------------
 async function login() {
